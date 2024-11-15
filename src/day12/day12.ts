396e89d7d8db45606c38a5e14088ef5693b16042
adam <adam@adam>
2024-11-14 22:53:22 -0300

    feat: day 12 - **

--- src/day12/day12.ts
+++ src/day12/day12.ts
@@ -1,6 +1,45 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write
+
+
 const fileName = "input.txt";
-const filePath = new URL(fileName, import.meta.url).pathname;
+const filePath = new URL(fileName, import.meta.url);
 const input = Deno.readTextFileSync(filePath).trim();
+
+function calcula(objeto: Array<object> | object ): number {
+  let resultado = 0;
+  // verifica se o objeto é uma lsita ou um dicionario
+  if (Array.isArray(objeto)) {
+    // Na lista itera sobre cada item da lista
+    for (const item of objeto) {
+      // verifica se o item da lista é outra lista ou um dicionario
+      if (typeof item === "object") {
+        // calcula via recursao
+        resultado += calcula(item);
+      } else {
+        // se o item da lista for um numero ele soma o numero
+        if (typeof item === "number") {
+          resultado += item
+        }
+      }
+    }
+    // retorna o resultado da lista
+    return resultado
+  } else {
+    // é um dicionari
+    for (const [_chave, valor] of Object.entries(objeto)) {
+      // verifica se o valor é um objeto
+      if (typeof valor === "object") {
+        resultado += calcula(valor)
+      } else if (valor === "red") {
+        return 0
+      } else if (typeof valor === "number") {
+        resultado += valor;
+      }
+    }
+    return resultado
+  }
+}
+
 
 export function solver(input: string, part: number) {
   const regex: RegExp = /[-]?[0-9]+/g;
@@ -10,20 +49,10 @@ export function solver(input: string, part: number) {
   if (part == 1) {
     return sumResult
   }
-  // const redRegex: RegExp = /{.*?red.*?}/g;
-  // const redRegex: regExp = /\{[^{}]*?"red"[^{}]*?\}/g;
-  const redMatch: string[] = input.match(redRegex) as string[];
+  const lista = JSON.parse(input);
   
-  let redNumber: number = 0
+  return calcula(lista);
 
-  redMatch.forEach(element => {
-    const lNum: string[] = (element.match(regex) as string[] || ["0"]);
-    console.log(element)
-    console.log(lNum)
-    const sumNum: number = lNum.map(Number).reduce((acc, item) => acc + item,0);
-    redNumber += sumNum;
-  });
-  return sumResult - redNumber;
 }
 
 console.log('parte 1:', solver(input, 1));
