5d3b7b79fd7b397ad8f17544c3049973f9a5cca5
adam <adam@adam>
2024-11-15 17:01:33 -0300

    feat: day 13 all stars **

--- src/day13/day13.ts
+++ src/day13/day13.ts
@@ -10,7 +10,8 @@ interface mapaFelicidade {
   };
 }
 
-export function solver(input: string, part: number) {
+export function solver(input: string, part: number): number {
+  // Parsing da entrada saindo um dicionario.
   const listaFelicidade: mapaFelicidade = {};
   for (const line of input.split("\n")) {
     const item = line.split(" ");
@@ -28,8 +29,53 @@ export function solver(input: string, part: number) {
     listaFelicidade[ultimoNome][item[0]] =
       (listaFelicidade[ultimoNome][item[0]] | 0) + felicidade;
   }
-  console.log(listaFelicidade);
+
+  if (part === 2) {
+    const tempListaPessoas = Object.keys(listaFelicidade);
+    listaFelicidade["eu"] = {};
+    for (const pessoa of tempListaPessoas) {
+      listaFelicidade[pessoa]["eu"] = 0;
+      listaFelicidade["eu"][pessoa] = 0;
+    }
+    // console.log(listaFelicidade);
+  }
+
+  // Gera a lista de possibilidades de pessoas sentadas.
+  const listaPessoas: string[] = Object.keys(listaFelicidade);
+  const listaPessoasSentadas: string[][] = [];
+  const pessoasSentadas: string[] = [listaPessoas.pop() ?? ""];
+
+  const pilha: [string[][]] = [[pessoasSentadas, listaPessoas]];
+  while (pilha.length > 0) {
+    const [pessoasSentadas, restantePessoas] = pilha.pop() ?? [[], []];
+    if (restantePessoas.length === 0) {
+      listaPessoasSentadas.push(pessoasSentadas);
+    }
+    restantePessoas.forEach((pessoa, index) => {
+      const tempPessoasSentadas: string[] = [...pessoasSentadas, pessoa];
+      const tempRestantePessoas: string[] = [
+        ...restantePessoas.slice(0, index),
+        ...restantePessoas.slice(index + 1),
+      ];
+      pilha.push([tempPessoasSentadas, tempRestantePessoas]);
+    });
+  }
+  //ToDo: Calcular a felicidade.
+  const valorFelicidade: number[] = [];
+
+  listaPessoasSentadas.forEach((lista) => {
+    const resultado = lista.reduce((acc, pessoa, index) => {
+      if (index == lista.length - 1) {
+        acc += listaFelicidade[lista[0]][pessoa];
+      } else {
+        acc += listaFelicidade[pessoa][lista[index + 1]];
+      }
+      return acc;
+    }, 0);
+    valorFelicidade.push(resultado);
+  });
+  return Math.max(...valorFelicidade);
 }
 
 console.log("parte 1:", solver(input, 1));
-// console.log('parte 2:', solver(input, 2));
+console.log("parte 2:", solver(input, 2));
